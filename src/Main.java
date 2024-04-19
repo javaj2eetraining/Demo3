b7e08361943cb00e3f3727321e80e730a83c59b3
saritha <aitham.saritha@gmail.com>
2024-04-19 21:27:03 +0530

    added sum method

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,11 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    public void sum() {
+        int x = 10;
+        int y = 20;
+        System.out.println(x + y);
+    }
+
 }
